1b1c124425ce147463aa59bdea60d8a2849add4c
Edgar J San Martin <edgar.sanmartinjr@gmail.com>
2021-05-15 19:07:20 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+/*
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://dad-jokes.p.rapidapi.com/random/joke");
+
+req.headers({
+	"x-rapidapi-key": process.env.RAPID_API_KEY,
+	"x-rapidapi-host": "dad-jokes.p.rapidapi.com",
+	"useQueryString": true
+});
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
+*/
