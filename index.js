4d28a652c3244e7eb736de738c4f048efc521ec6
Edgar J San Martin <edgar.sanmartinjr@gmail.com>
2021-05-16 15:16:16 -0400

    Figured out the timing thing, going to take out toad-scheduler for cron

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const twilio = require('twilio');
-const { ToadScheduler, SimpleIntervalJob, AsyncTask } = require('toad-scheduler');
 const cron = require('cron');
 const { setupTextContent,
         setupTextDetail,
@@ -20,37 +19,27 @@ let accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.tw
 let authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.twilio.com/console
 const client = twilio(accountSid, authToken);
 
-// lets text be sent once a day
-const scheduler = new ToadScheduler();
-const task = new AsyncTask(
-    'simple task',
-    async () => {
-        try {
-            let dadJoke = {};
-            getDadJokeAsync(function(error, res){
-                if(error != null){
-                    console.error(`Error setting dadJokeObject: \t ${error}`)
-                } else {
-                    dadJoke.setup = res.setup;
-                    dadJoke.punchline = res.punchline;
-                    let textContent = setupTextContent(dadJoke);
-                    let text = setupTextDetail(textContent);
-                    client.messages.create(text);
-                }
-            });
-        } catch(err){
-            console.error(`Error ocurred trying to text you: ${err}`);
-        }
-    },
-    (err) => { console.error(`Error ocurred running this daily task: ${err}`); }
-);
-const job = new SimpleIntervalJob({ seconds: 10 }, task)
-
 // Starts this program at 7am. Then toad-scheduler will re-run the text program every day
 let CronJob = cron.CronJob;
-let startJob = new CronJob('10 15 * * *', function(){
+let job = new CronJob('20 15 * * *', async function(){
     console.log('Cron Job starting');
-    scheduler.addSimpleIntervalJob(job);
+    try {
+        let dadJoke = {};
+        getDadJokeAsync(function(error, res){
+            if(error != null){
+                console.error(`Error setting dadJokeObject: \t ${error}`)
+            } else {
+                dadJoke.setup = res.setup;
+                dadJoke.punchline = res.punchline;
+                let textContent = setupTextContent(dadJoke);
+                let text = setupTextDetail(textContent);
+                client.messages.create(text);
+            }
+        });
+    } catch(err){
+        console.error(`Error ocurred trying to text you: ${err}`);
+    }
 }, null, true, 'America/New_York');
-startJob.start();
+job.start();
  
+//'0 7 * * 0-6'
